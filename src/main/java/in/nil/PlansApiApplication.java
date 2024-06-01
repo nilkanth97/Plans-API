0d6458ddb75e953903fb20f140ea38df38d5199b
Nilkanth Narkhede <nilkanth97narkhede@gmail.com>
2024-06-01 22:37:02 +0530

    Update PlansApiApplication.java

--- src/main/java/in/nil/PlansApiApplication.java
+++ src/main/java/in/nil/PlansApiApplication.java
@@ -8,6 +8,7 @@ public class PlansApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlansApiApplication.class, args);
+		System.out.println("Nilkant Github Repo successfull");
 	}
 
 }
